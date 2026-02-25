8cabcd14e9f6c03abfe2eb89239e90578c6db101
kelvinhovinho <kelvinadamba@hotmail.com>
2026-02-26 00:44:18 +0300

    Finished linking the templates files

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('register', views.RegisterView, name='register'),
+    path('login/', views.LoginView, name='login')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello World !!!</h1>")
+    return render(request, 'core/index.html')
+
+def RegisterView(request):
+    return render(request, 'core/register.html')
+
+def LoginView(request):
+    return render(request, 'core/login.html')
