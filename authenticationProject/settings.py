796e788cb3b0a435fd63d8d4345f27a76f9a0361
Akelvino <vinho892@gmail.com>
2026-03-20 22:51:10 +0300

    Added static settings

--- authenticationProject/settings.py
+++ authenticationProject/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-TATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -136,13 +138,3 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vinho892@gmail.com'
 EMAIL_HOST_PASSWORD = 'dvnufrjinhwlasgq'
 DEFAULT_FROM_EMAIL = 'vinho892@gmail.com'
-
-# settings.py
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'your_email@gmail.com' # Your full Gmail address
-# EMAIL_HOST_PASSWORD = 'your_app_password' # The 16-character app password
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
